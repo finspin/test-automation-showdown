f88a51d04d1753bb03ece79d94d5f07e71daa17b
finspin <jarospisak@gmail.com>
2017-12-09 19:59:13 +0200

    Fix python tests so that BrowserStack session is launched only once

--- python/test.py
+++ python/test.py
@@ -4,9 +4,10 @@ from selenium import webdriver
 
 class Login(unittest.TestCase):
 
-    def setUp(self):
-        self.base_url = "http://www.testmeplease.site"
-        self.desired_caps = {
+    @classmethod
+    def setUpClass(cls):
+        cls.base_url = "http://www.testmeplease.site"
+        cls.desired_caps = {
             "browserstack.user": os.environ["BROWSERSTACK_USER"],
             "browserstack.key": os.environ["BROWSERSTACK_KEY"],
             "build": os.environ["TRAVIS_BUILD_NUMBER"],
@@ -16,45 +17,50 @@ class Login(unittest.TestCase):
             "browser_version": "62.0",
             "resolution": "1024x768"
         }
-        self.driver = webdriver.Remote(
-            command_executor="http://hub-cloud.browserstack.com/wd/hub",
-            desired_capabilities=self.desired_caps
+        cls.driver = webdriver.Remote(
+            command_executor = "http://hub-cloud.browserstack.com/wd/hub",
+            desired_capabilities = cls.desired_caps
         )
 
-    def test_should_successfully_log_in(self):
-        base_url = self.base_url
+    @classmethod
+    def test_should_successfully_log_in(cls):
+        driver = cls.driver
+        base_url = cls.base_url
 
-        self.driver.get(base_url + "/login")
+        driver.get(base_url + "/login")
 
-        username = self.driver.find_element_by_id("username")
-        password = self.driver.find_element_by_id("password")
-        submit_button = self.driver.find_element_by_css_selector('button[type="submit"]')
-
-        username.send_keys("user")
-        password.send_keys("pass")
-        submit_button.click()
-
-        assert self.driver.current_url == base_url + "/profile"
-
-    def test_should_successfully_log_out(self):
-        base_url = self.base_url
-
-        self.driver.get(base_url + "/login")
-
-        username = self.driver.find_element_by_id("username")
-        password = self.driver.find_element_by_id("password")
-        submit_button = self.driver.find_element_by_css_selector('button[type="submit"]')
+        username = driver.find_element_by_id("username")
+        password = driver.find_element_by_id("password")
+        submit_button = driver.find_element_by_css_selector('button[type="submit"]')
 
         username.send_keys("user")
         password.send_keys("pass")
         submit_button.click()
 
-        self.driver.find_element_by_css_selector('a[href="/logout"]').click()
+        assert driver.current_url == base_url + "/profile"
 
-        assert self.driver.current_url == base_url + "/login"
+    @classmethod
+    def test_should_successfully_log_out(cls):
+        driver = cls.driver
+        base_url = cls.base_url
 
-    def tearDown(self):
-        self.driver.quit()
+        driver.get(base_url + "/login")
+
+        username = driver.find_element_by_id("username")
+        password = driver.find_element_by_id("password")
+        submit_button = driver.find_element_by_css_selector('button[type="submit"]')
+
+        username.send_keys("user")
+        password.send_keys("pass")
+        submit_button.click()
+
+        driver.find_element_by_css_selector('a[href="/logout"]').click()
+
+        assert driver.current_url == base_url + "/login"
+
+    @classmethod
+    def tearDownClass(cls):
+        cls.driver.quit()
 
 if __name__ == "__main__":
     unittest.main()
